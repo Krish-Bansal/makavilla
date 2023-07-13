a0b166b05987a097a0996fdeffdfa4cd44980618
Yukta Bansal <yuktabansal11@gmail.com>
2023-07-13 09:55:07 +0545

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,10 @@ const toggleNavbar = function () {
 addEventsOnElements(navTogglers, "click", toggleNavbar)
 
 
-const header = document.querySelector("[data-header]");
+// Header & Back to Top 
 
+const header = document.querySelector("[data-header]");
+const backTopBtn=document.querySelector("[data-back-top-btn]")
 let lastScrollPos = 0;
 
 
@@ -54,10 +56,14 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
+
     hideHeader()
 
   } else {
     header.classList.remove("active")
+    backTopBtn.classList.remove("active")
+
   }
 })
 
